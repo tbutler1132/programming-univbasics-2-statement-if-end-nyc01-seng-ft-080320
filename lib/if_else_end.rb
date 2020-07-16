299e2fedd09b6fcd61bead4b4385bbb6b8cc83e7
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-16 14:42:29 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,12 +1,11 @@
 current_time = Time.now
 
-current_time.to_i
+current_time = current_time.to_i
 
-def even?
+
  if current_time % 2 == 0
   puts "Even!"
  else
   puts "Odd!"
  end
-end
 # Write your solution here
